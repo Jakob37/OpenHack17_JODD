b346e87abd8211aee7512cd5a0c65dfeb7b467b2
danielhamngren <daniel@memoto.com>
2017-10-08 09:28:30 +0200

    Added tags in existind current information

--- backend/backend.py
+++ backend/backend.py
@@ -11,16 +11,16 @@ def hello_world():
     return 'Hello, world!'
 
 events = [
-    {'date':'2017-10-25', 'data':'Fell ill after eating .'},
-    {'date':'2017-10-16', 'data':'Got scared by a car yesterday, had trouble calming down. '
+        {'date':'2017-10-25', 'tags':['day care', 'medical care'], 'data':'Fell ill after eating .'},
+        {'date':'2017-10-16', 'tags':['day care'], 'data':'Got scared by a car yesterday, had trouble calming down. '
                                  'Take it extra slowly when passing the road.'},
-    {'date':'2017-10-08', 'data':'Teeth are much better now. Should be able to eat '
+        {'date':'2017-10-08', 'tags':['family', 'day care'], 'data':'Teeth are much better now. Should be able to eat '
                                  'normally by now.'},
-    {'date':'2017-10-06', 'data':'Kalle has had trouble with his teeth during the night.'
+        {'date':'2017-10-06', 'tags':['family', 'day care'], 'data':'Kalle has had trouble with his teeth during the night.'
                                  ' He has not eaten properly, and is very tired. '
                                  'Grandmother is available today if he needs to be '
                                  'picked up extra early.'},
-    {'date':'2017-10-05', 'data':'Kalle has been visiting the dentist. He is extra tired '
+        {'date':'2017-10-05', 'tags':['day care'], 'data':'Kalle has been visiting the dentist. He is extra tired '
                                  'today. He should eat an extra banana after lunch. '
                                  'May be angry in the afternoon.'}
 ]
